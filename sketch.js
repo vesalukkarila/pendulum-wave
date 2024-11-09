4eb57b49883b9fc30ec15a8d53321db094d8c3e4
Vesa Lukkarila <github@vesalukkarila.com>
2024-11-09 22:17:21 +0200

    Add ball to the end of pendulum

--- sketch.js
+++ sketch.js
@@ -1,26 +1,31 @@
 
+//window
 let width;
 let height;
-let radius;
+//pendulum string
+let lengthOfPendulum;
 let startAngle;
 let endAngle;
 let angle;
 let changeOfAngle;
 let swingingLeft;
-//let ball;
+//ball at the end of pendulum
+let ball;
+let ballRadius;
 
 
 function setup() {
   //define global variables
   width = 600;
   height = 600;
-  radius = 200;
+  lengthOfPendulum = 200;
   startAngle = PI/4;
   endAngle = 3*PI/4;
   angle = startAngle;
-  changeOfAngle = PI/180;
+  changeOfAngle = 0.01;
   swingingLeft = true;
-
+  ball = new Ball(0, 0, 0);
+  ballRadius = 20;
   // 
   createCanvas(width, height);
   background(20);
@@ -30,14 +35,41 @@ function setup() {
 function draw() {
   background(20);
 
-  //change angle
+  changeAngleOfPendulum();
+
+  //calculate this frame's x and y based on angle
+  let endX = width/2 + cos(angle) * lengthOfPendulum;
+  let endY =  sin(angle) * lengthOfPendulum;
+  //draw
+  stroke("Aquamarine");
+  strokeWeight(1);
+  line(width/2, 0, endX, endY);
+  ball.draw(endX, endY, ballRadius);
+}
+
+
+
+class Ball{
+  constructor(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+  
+  draw(x, y, radius){
+    fill(20);
+    circle(x, y, radius);
+  }
+}
+
+function changeAngleOfPendulum(){
   if (swingingLeft && angle < endAngle){
-      //increase angle by X
-      angle += changeOfAngle;
+    //increase angle by X
+    angle += changeOfAngle;
   }else if (swingingLeft && angle >= endAngle){
-    //change swinginLeft to false and decresease angle by X
-    swingingLeft = false;
-    angle -= changeOfAngle;
+  //change swinginLeft to false and decresease angle by X
+  swingingLeft = false;
+  angle -= changeOfAngle;
   }
 
   if (!swingingLeft && angle > startAngle){
@@ -47,18 +79,6 @@ function draw() {
     //change swingingLeft to true and increase angle X
     swingingLeft = true;
     angle += changeOfAngle;
-
   }
-
-
-  //calculate this frame's x and y based on angle
-  let endX = width/2 + cos(angle) * radius;
-  let endY =  sin(angle) * radius;
-  //draw
-  stroke("Aquamarine");
-  strokeWeight(1);
-  line(width/2, 0, endX, endY);
 }
-
-
 
