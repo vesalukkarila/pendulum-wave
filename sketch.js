88160a26387296964913aee7c7642b8d17b8237e
Vesa Lukkarila <github@vesalukkarila.com>
2024-11-23 15:41:13 +0200

    swinging with acceleration

--- sketch.js
+++ sketch.js
@@ -1,33 +1,26 @@
-// TODOs
-/* 
-1. Put natural swing to it
-- changOfAngle 0 at pi/4 and 3pi/4, changOfAngle grows to e.g. 0.1 towards pi/2 from both directions
-- i.e. 0 -> 0.1
-*/
-
-
 //canvas
 let width;
 let height;
 //pendulum wire
 let lengthOfPendulum;
-let startAngle;
-let endAngle;
-let angle;
 let changeOfAngle;
 let swingingLeft;
+let startAngle;
+let middlePoint;
+let endAngle;
+let currentAngle;
 //ball at the end of pendulum
 let ball;
 let ballRadius;
 
-
 function setup() {
   //pendulum wire
   lengthOfPendulum = 200;
-  startAngle = PI/4;
-  endAngle = 3*PI/4;
-  angle = startAngle;
-  changeOfAngle = 0.01;
+  startAngle = PI / 4;
+  middlePoint = PI / 2;
+  endAngle = (3 * PI) / 4;
+  currentAngle = startAngle;
+  changeOfAngle = 0.01; //rename
   swingingLeft = true;
   //Initiate Ball at the end of pendulum
   ball = new Ball(0, 0, 0);
@@ -37,54 +30,92 @@ function setup() {
   height = 600;
   createCanvas(width, height);
   background(20);
-  
 }
 
 function draw() {
   background(20);
 
-  changeAngleOfPendulum();
+  changeAngleOfPendulumWithNaturalSwing();
 
   //calculate this frame's x and y based on angle
-  let endX = width/2 + cos(angle) * lengthOfPendulum;
-  let endY =  sin(angle) * lengthOfPendulum;
+  let endX = width / 2 + cos(currentAngle) * lengthOfPendulum;
+  let endY = sin(currentAngle) * lengthOfPendulum;
   //draw
   stroke("Aquamarine");
   strokeWeight(1);
-  line(width/2, 0, endX, endY);
+  line(width / 2, 0, endX, endY);
   ball.draw(endX, endY, ballRadius);
 }
 
-
-
-class Ball{
-  constructor(x, y, radius){
+class Ball {
+  constructor(x, y, radius) {
     this.x = x;
     this.y = y;
     this.radius = radius;
   }
-  
-  draw(x, y, radius){
+
+  draw(x, y, radius) {
     fill(20);
     circle(x, y, radius);
   }
 }
+// TODOs
+/* 
+1. Put natural swing to it
+- changOfAngle 0 at pi/4 and 3pi/4, changOfAngle grows to e.g. 0.1 towards pi/2 from both directions
+- i.e. 0 -> 0.1
+*/
+function changeAngleOfPendulumWithNaturalSwing() {
+  let mappedAngle = map(currentAngle, PI / 4, (3 * PI) / 4, 0.001, 0.03);
+  let speed = mappedAngle;
+  if (speed >= (0.0005 + 0.03) / 2) {
+    speed = 0.03 - mappedAngle + 0.0005;
+  }
 
-function changeAngleOfPendulum(){
-  if (swingingLeft && angle < endAngle){
-    //increase angle by X
-    angle += changeOfAngle;
-  }else if (swingingLeft && angle >= endAngle){
-  //change swinginLeft to false and decresease angle by X
-  swingingLeft = false;
-  angle -= changeOfAngle;
-  }else if (!swingingLeft && angle > startAngle){
-    //decrease angle by X
-    angle -= changeOfAngle;
-  }else if (!swingingLeft && angle <= startAngle){
-    //change swingingLeft to true and increase angle X
+  changeAngleOfPendulum(speed);
+}
+
+function changeAngleOfPendulum(speed) {
+  if (
+    swingingLeft &&
+    currentAngle >= startAngle &&
+    currentAngle <= middlePoint
+  ) {
+    console.log("vasemmalle eka puolisko");
+    currentAngle += speed;
+  } else if (
+    swingingLeft &&
+    currentAngle >= startAngle &&
+    currentAngle > middlePoint &&
+    currentAngle < endAngle
+  ) {
+    console.log("vasemmalle toka puolisko");
+    currentAngle += speed;
+  } else if (swingingLeft && currentAngle >= endAngle) {
+    console.log("Päätepysäkki saavutettu");
+    swingingLeft = false;
+    currentAngle === endAngle;
+    currentAngle -= speed;
+  } else if (
+    !swingingLeft &&
+    currentAngle <= endAngle &&
+    currentAngle >= middlePoint
+  ) {
+    console.log("toka takas");
+    currentAngle -= speed;
+  } else if (
+    !swingingLeft &&
+    currentAngle < middlePoint &&
+    currentAngle > startAngle
+  ) {
+    console.log("eka takas");
+    currentAngle -= speed;
+  } else if (!swingingLeft && currentAngle <= startAngle) {
+    console.log("alku saavutettu");
     swingingLeft = true;
-    angle += changeOfAngle;
+    currentAngle === startAngle;
+    currentAngle += speed;
+    console.log(speed);
   }
 }
 
@@ -97,4 +128,3 @@ moves faster than the one predecing it by exactly one swing over the course of t
 
 //   return 9.81*(waveDurationInSeconds/(2 * PI * (lenghtOfLongest + numberFromLongest + 1)));
 // }
-
