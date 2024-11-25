fb5c71f4efa507b2c4cfb1bc56c37be6c09c8d3b
Vesa Lukkarila <github@vesalukkarila.com>
2024-11-25 08:10:53 +0200

    clean code to OO style

--- sketch.js
+++ sketch.js
@@ -2,34 +2,22 @@
 let width;
 let height;
 //pendulum wire
-let lengthOfPendulum;
-let changeOfAngle;
-let swingingLeft;
-let startAngle;
-let middlePoint;
-let endAngle;
+let lengthOfPendulum = 200;
 let currentAngle;
-//ball at the end of pendulum
+//ball itself
+let ballRadius = 20;
 let ball;
-let ballRadius;
-
-//new ones
+let ball2;
+//forces to play with
 let angleAcceleration = 0;
 let angleVelocity = 0;
 let gravity = 0.3;
 
 function setup() {
-  //pendulum wire
-  lengthOfPendulum = 200;
-  startAngle = PI / 4;
-  middlePoint = PI / 2;
-  endAngle = (3 * PI) / 4;
-  currentAngle = startAngle;
-  changeOfAngle = 0.01; //rename
-  swingingLeft = true;
-  //Initiate Ball at the end of pendulum
-  ball = new Ball(0, 0, 0);
-  ballRadius = 20;
+  currentAngle = PI / 4;
+
+  ball = new Ball(ballRadius, currentAngle, lengthOfPendulum);
+  ball2 = new Ball(ballRadius, currentAngle, lengthOfPendulum + 50);
   // canvas setup
   width = 600;
   height = 600;
@@ -39,114 +27,43 @@ function setup() {
 
 function draw() {
   background(20);
-
-  //changeAngleOfPendulumWithNaturalSwing();
-  swing();
-  //calculate this frame's x and y based on angle
-  let endX = width / 2 + sin(currentAngle) * lengthOfPendulum; //vaihdettu siniks
-  let endY = cos(currentAngle) * lengthOfPendulum; //vaihdettu cosiks
-  //draw
-  stroke("Aquamarine");
-  strokeWeight(1);
-  line(width / 2, 0, endX, endY);
-  ball.draw(endX, endY, ballRadius);
-}
-
-function swing() {
-  let force = gravity * sin(currentAngle);
-  console.log("force: " + force);
-  angleAcceleration = (-1 * force) / lengthOfPendulum; //ehkä eri lailla
-  console.log("acceleration: " + angleAcceleration);
-  angleVelocity += angleAcceleration;
-  console.log("velocity: " + angleVelocity);
-
-  currentAngle += angleVelocity;
-  console.log("currentAngle: " + currentAngle);
-  console.log("   ");
+  ball.swing();
+  ball2.swing();
+  ball.updatePosition();
+  ball2.updatePosition();
+  ball.draw();
+  ball2.draw();
 }
 
 class Ball {
-  constructor(x, y, radius) {
-    this.x = x;
-    this.y = y;
+  constructor(radius, currentAngle, lengthOfPendulum) {
+    this.x;
+    this.y;
     this.radius = radius;
+    this.currentAngle = currentAngle;
+    this.lengthOfPendulum = lengthOfPendulum;
+    this.angleAcceleration;
+    this.angleVelocity = 0;
+    this.updatePosition();
   }
 
-  draw(x, y, radius) {
+  swing() {
+    this.force = gravity * sin(this.currentAngle);
+    this.angleAcceleration = (-1 * this.force) / this.lengthOfPendulum;
+    this.angleVelocity += this.angleAcceleration;
+    this.currentAngle += this.angleVelocity;
+  }
+
+  updatePosition() {
+    this.x = width / 2 + sin(this.currentAngle) * this.lengthOfPendulum;
+    this.y = cos(this.currentAngle) * this.lengthOfPendulum;
+  }
+
+  draw() {
+    stroke("Aquamarine");
+    strokeWeight(1);
+    line(width / 2, 0, this.x, this.y);
     fill(20);
-    circle(x, y, radius);
+    circle(this.x, this.y, this.radius);
   }
-}
-// TODOs
-/* 
-1. swing could be more gravity-created-like
-2. Stopping at the ends doesn't look natural
-*/
-/*
-function changeAngleOfPendulumWithNaturalSwing() {
-  let mappedAngle = map(currentAngle, PI / 4, (3 * PI) / 4, 0.001, 0.03);
-  let speed = mappedAngle;
-  if (speed >= (0.0005 + 0.03) / 2) {
-    speed = 0.03 - mappedAngle + 0.0005;
-  }
-
-  changeAngleOfPendulum(speed);
-}
-
-function changeAngleOfPendulum(speed) {
-  if (
-    swingingLeft &&
-    currentAngle >= startAngle &&
-    currentAngle <= middlePoint
-  ) {
-    console.log("vasemmalle eka puolisko");
-    currentAngle += speed;
-  } else if (
-    swingingLeft &&
-    currentAngle >= startAngle &&
-    currentAngle > middlePoint &&
-    currentAngle < endAngle
-  ) {
-    console.log("vasemmalle toka puolisko");
-    currentAngle += speed;
-  } else if (swingingLeft && currentAngle >= endAngle) {
-    console.log("Päätepysäkki saavutettu");
-    swingingLeft = false;
-    currentAngle === endAngle;
-    currentAngle -= speed;
-  } else if (
-    !swingingLeft &&
-    currentAngle <= endAngle &&
-    currentAngle >= middlePoint
-  ) {
-    console.log("toka takas");
-    currentAngle -= speed;
-  } else if (
-    !swingingLeft &&
-    currentAngle < middlePoint &&
-    currentAngle > startAngle
-  ) {
-    console.log("eka takas");
-    currentAngle -= speed;
-  } else if (!swingingLeft && currentAngle <= startAngle) {
-    console.log("alku saavutettu");
-    swingingLeft = true;
-    currentAngle === startAngle;
-    currentAngle += speed;
-    console.log(speed);
-  }
-}*/
-
-// TODO
-/* This equation outputs pendulum lengths, such that, each pendulum moving backward in the series (from longest to shortest)
-moves faster than the one predecing it by exactly one swing over the course of the 'wave' duration
-(the amount of time the pattern repeats itself)*/
-function lengthOfThisPendulum(lenghtOfLongest, numberFromLongest) {
-  let waveDurationInSeconds = 90;
-
-  return (
-    9.81 *
-    (waveDurationInSeconds /
-      (2 * PI * (lenghtOfLongest + numberFromLongest + 1)))
-  );
 }
